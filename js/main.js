473eebf764288020490fd2e9686ea82e02903570
Viola Hu <lovelyviolaat@hotmail.com>
2019-05-21 17:03:31 +1000

    in progress of defactor, still going on

--- js/main.js
+++ js/main.js
@@ -13,19 +13,63 @@ const board = [
 // which can be used to make sure that X and O will take turns to be put into boxes
 let validClickNumber = 0;
 
+
+// still need to put output result inside the click event callback, but as it's just a repetitive thing, can write it outside the event in a function, and when it gets to return result, can call the function from inside the event callback.
+
+const outputResult = function (winningOrNot) {
+
+  if (winningOrNot === true) {
+    $('.hidden').html('Winning!!!').css('visibility','visible');
+    $('td').off('click');
+  } else {
+    $('.hidden').html('Oooops! No luck!').css('visibility','visible');
+    $('td').off('click');
+  }
+};
+//************************
+// put a game handler to stop the game once the click event returns a result, either winning or no winner at all!
+// #1, if use $('td').off('click'); to turn off the event, once this round of game is finished, click is not working anymore on the page, as it's turned off. Users will need to refresh the page and restart.
+//The whole above process is no problem for only one round of game!!
+//#2, However, if you are planning on running the game for multiple rounds without refreshing the page, for example, click a 'button' to restart game inside the same page, it will require to turn on the 'click' event again, as the $('td') element's click event is turned off in the DOM by the last round! which requires more steps to turn it back on.
+//#3, Thus good practice is not to turn off the event, instead, add another if-statement inside the event to test at the beginning if the game has finished/returned or not, by using the variable 'winningOrNot' to control the flow!
+//#4, Meanwhile, the variable winningOrNot should stay outside of the click event so that it stores the event info after every click instead of being reset when starting a new click!!
+
+
+
+// Q: if call the above function to turn off the click event, then inside the checking loop, is 'return' at the end still necessary? or even without 'return', nothing further would happen?
+// A: return is needed! turn off the event trigger only means, for the next click, nothing would happen as the event is turned off. But for the current round of click event, after this outputResult function is called, it will continue to run the following code in the original event callback function if there's no return!
+//**************************
+
 $('td').on('click', function(e){
-  let xOrY = '';
+//----- Step 1, on each click, show input on UI, make sure duplicate clicks handled -----
+
+  let xOrY; // store the UI input, X or Y, into a variable for later use!
+
+  if ($(this).html() !== "" ){ // click on any box, if it's not empty - occupied already, return message!
+  $('h2').html('The space is occupied!')
+
+  //-----------------------------------------------------
+    // $('.hidden').css('visibility','visible');
+    // instead of using hide/show contents by CSS, can simply add an empty element 'h2' at the beginning, and later add or change .html() contents inside it. Thus, there's no need to change CSS that's separated in another file then.
+  //-----------------------------------------------------
+    return;  // if a space is occupied, no need to run the rest code, return callback directly! so can start from the next new click.
+  }
+  //
+  // if( winningOrNot) {
+  //
+  //   return;
+  // }
 
 
+  $('h2').html(' '); // in case last click made the hidden part to visible, reset here! if it's still hidden, no harm!
+  //Q: is this a good approach? kind of repetitive!
+  //A: YES, it's necessary, like reset!!!
 
 
-  if ($(this).html() !== ""){ // click on any box, if it's not empty, return alert!
-    alert("This space is occupied!")
-  } else
-  if (validClickNumber % 2 === 0){ // if empty, when validClickNumber is even, input 'X', when odd, input 'O'. As validClickNumber starts from 0, so the first input is 'X'
-    validClickNumber += 1; //update the total number of valid clicks
-    $(this).html('X'); //add 'X' to the clicked td element as content
-    xOrY = 'X'; //update xOrY's value, to be used for storing click content into variable later
+  if (validClickNumber % 2 === 0){ // Otherwise, if it's empty, when validClickNumber is even, input 'X', when odd, input 'O'. As validClickNumber starts from 0, so the first input is always 'X'
+    validClickNumber += 1; // update the total number of valid clicks
+    $(this).html('X'); // add 'X' to the clicked td element as content
+    xOrY = 'X'; // update xOrY variable's value, to be used for storing click content into board array later
   } else {
     validClickNumber += 1;
     $(this).html('O');
@@ -33,42 +77,42 @@ $('td').on('click', function(e){
   }
   // console.log(xOrY); //OK
 
-
-  // get the click event object data - target property, it prints out the pure DOM element that you just clicked, including all its ATTRIBUTE information - id attribute!
+  //----- Step 2, store the UI input variable into board array --------
+  // get the click event object data - target property, it prints out the pure DOM element that user just clicked, including all its ATTRIBUTEs and contents!
+  // store this target DOM element into a variable to utilise its id attribute!!!
   const clickedTarget = e.target;
   // console.log(clickedTarget); // print out the clicked td element // OK
   // console.log($(clickedTarget).attr('id')); // print out the id of the td element, which is a string 'X Y' // OK
   // console.log(typeof($(clickedTarget).attr('id'))); // confirmed it's a string // OK
 
 
-  // get the id attribute from the event target property, whose value is a string with two numbers,
-  // and split them into an array with two string items of number.
+  // from the event target DOM element, get the id attribute, whose value is a string with two numbers, like 'X Y'
+  // and split them into an array with two string items of number, like ['X','Y'], and store it into a variable to avoid repetition.
   const location = $(clickedTarget).attr('id').split(" ")
   // console.log(location); // OK
-  const locationX = parseInt(location[0]); // pure number for location X
-  const locationY = parseInt(location[1]); // pure number for location Y
+  const locationX = parseInt(location[0]); // pure number for location X, from 0 - 2
+  const locationY = parseInt(location[1]); // pure number for location Y, from 0 - 2
   // console.log(locationX, 'and', locationY); // OK
 
-  // use the attribute value X and Y to locate where in the 2D array to store the click input
-  // need to try avoid user click into the occupied place and change the value there!!!
+  // use the attribute value X and Y to locate where in the 2D array to store the click content
+  // need to avoid user clicking into the occupied place and changing the value there!!!
   if (board[locationX][locationY] === '') {
     board[locationX][locationY] = xOrY;
   }
   // console.table(board); //OK
-  //so far, corretly store the click X or O into the right position of the array!
+  // so far, corretly store the click content 'X' or 'O' into the right position of the board array!
 
 
   //*************** Core Logic *******************
-  //use logic to judge if succeed or not!!!
-
-  //loop through the whole 2D array to see if there's any item matching this pass-in value on 4x directions ‘米 ’
-  // if the total matching number === 3, then winner!!
+  // use logic to judge if succeed or not!!!
+  // loop through the whole 2D array to see if there's any existing item matching this pass-in click content on its 4x directions ‘米 ’.
+  // if the total matching number on any of the 4x directions === board.length(i.e. in this case is 3), then winner!!
   // this logic should work for any size of board(n * n)
 
 
-  // on each direction only need to loop 'board.length'x times
+  // on each direction only need to loop 'board.length'x times, store it in a variable for multiple uses
   const boardLength = board.length;
-  let winningOrNot = false;
+  let winningOrNot = false; // this is to store return value from the click event callback function, to pass its final value to UI for further result display.
 
   //******* 1, loop through horizontal direction *******
   const arrayForCheck = board[locationX];
@@ -83,8 +127,9 @@ $('td').on('click', function(e){
   // console.log(matchingNumberHorizontal); //OK
   if (matchingNumberHorizontal === boardLength){
     winningOrNot = true;
-    console.log('winning!');
-    return winningOrNot;
+    outputResult(winningOrNot);
+    // console.log('winning!'); //OK
+    return ;
   }
   //
   //
@@ -100,8 +145,9 @@ $('td').on('click', function(e){
   // console.log(matchingNumberVertical); //OK
   if (matchingNumberVertical === boardLength){
     winningOrNot = true;
-    console.log('winning!');
-    return winningOrNot;
+    outputResult(winningOrNot);
+    // console.log('winning!');//OK
+    return ;
   }
 
 
@@ -121,8 +167,9 @@ $('td').on('click', function(e){
     // console.log(matchingNumberBackwardDiagnal); //OK
     if (matchingNumberBackwardDiagnal === boardLength){
       winningOrNot = true;
-      console.log('winning!');
-      return winningOrNot;
+      outputResult(winningOrNot);
+      // console.log('winning!');//OK
+      return ;
     }
   }
 
@@ -130,7 +177,9 @@ $('td').on('click', function(e){
   //******* 4, loop through forward diagonal direction '/' *******
   // if this input's (locationX + locationY) !== (array.length - 1), means, it's not in the center forward diagonal line,
   // then there's no need to check forward diagonal direction!
-  // in that situtaion, winningOrNot remain false;
+  // as the only way for this kind of location to win is through horizontal and vertical line,
+  // which should have been checked on the above 2x steps!
+  // if not in the center forward diagonal line, after checking the above steps up to here, winningOrNot remains false;
   if ((locationX + locationY) === (boardLength - 1)){
     let matchingNumberForwardDiagnal = 0;
 
@@ -142,14 +191,34 @@ $('td').on('click', function(e){
     // console.log(matchingNumberForwardDiagnal); //OK
     if (matchingNumberForwardDiagnal === boardLength){
       winningOrNot = true;
-      console.log('winning!');
-      return winningOrNot;
+      outputResult(winningOrNot);
+      // console.log('winning!'); //OK
+      return;
     }
+  }
+
+  // the worst case scenerio, no winner at all, need to return
+  // after clicking all the spaces, the total number of spaces in the array should be (array.length ** 2),
+  // if there's any winner, it should have returned in previous steps, and the whole click event callback function finishes, also triggers the event to be off.
+  // if there's no winner, it will reach this step, but as it's included inside the event callback for each click, only when the valid click numbers reached (array.length ** 2), meanwhile there's no matching from the above steps, it can determine that there's no winner at the end, Game Over!
+  if (validClickNumber === boardLength ** 2){
+    outputResult(winningOrNot); // in this step, winningOrNot remain false!
+    return;
   }
 });
 
 // So far, the above logic should work for multiple line board,
-// need to think about output into DOM tomorrow
+// we got return value from the click event callback function, which also triggered the output on UI!
+// !!! the winningOrNot variable should sit outside the click event, then can use this returned value to UI intercation.
+
+
+
+
+
+
+  // 4>>>> put all check loops into separate functions, make click event callback look neat!
+
+// need to think about output into DOM tomorrow - Done!
 // also separate jQuery with vanilla JS
 // also further refactor
 // finally, decorate UI!
@@ -159,7 +228,6 @@ $('td').on('click', function(e){
 
 
 
-//1,
 // tic-tac-toe
 // Good Practice:
 
