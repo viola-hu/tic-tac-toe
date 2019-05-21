09731aca7c18828939f5a97ec29cd55b1af90eb6
Viola Hu <lovelyviolaat@hotmail.com>
2019-05-21 18:19:06 +1000

    in progress of defactor, still going on

--- js/main.js
+++ js/main.js
@@ -16,23 +16,26 @@ let validClickNumber = 0;
 
 // still need to put output result inside the click event callback, but as it's just a repetitive thing, can write it outside the event in a function, and when it gets to return result, can call the function from inside the event callback.
 
-const outputResult = function (winningOrNot) {
 
-  if (winningOrNot === true) {
-    $('.hidden').html('Winning!!!').css('visibility','visible');
-    $('td').off('click');
-  } else {
-    $('.hidden').html('Oooops! No luck!').css('visibility','visible');
-    $('td').off('click');
-  }
-};
-//************************
+
+//***********************************************
+// const outputResult = function (gameIsOver) {
+//
+//   if (gameIsOver === true) {
+//     $('.hidden').html('Winning!!!').css('visibility','visible');
+//     $('td').off('click');
+//   } else {
+//     $('.hidden').html('Oooops! No luck!').css('visibility','visible');
+//     $('td').off('click');
+//   }
+// };
+//***********************************************
 // put a game handler to stop the game once the click event returns a result, either winning or no winner at all!
 // #1, if use $('td').off('click'); to turn off the event, once this round of game is finished, click is not working anymore on the page, as it's turned off. Users will need to refresh the page and restart.
 //The whole above process is no problem for only one round of game!!
 //#2, However, if you are planning on running the game for multiple rounds without refreshing the page, for example, click a 'button' to restart game inside the same page, it will require to turn on the 'click' event again, as the $('td') element's click event is turned off in the DOM by the last round! which requires more steps to turn it back on.
-//#3, Thus good practice is not to turn off the event, instead, add another if-statement inside the event to test at the beginning if the game has finished/returned or not, by using the variable 'winningOrNot' to control the flow!
-//#4, Meanwhile, the variable winningOrNot should stay outside of the click event so that it stores the event info after every click instead of being reset when starting a new click!!
+//#3, Thus good practice is not to turn off the event, instead, add another if-statement inside the event to test at the beginning if the game has finished/returned or not, by using the variable 'gameIsOver' to control the flow!
+//#4, Meanwhile, the variable gameIsOver should stay outside of the click event so that it stores the event info after every click instead of being reset when starting a new click!!
 
 
 
@@ -40,30 +43,43 @@ const outputResult = function (winningOrNot) {
 // A: return is needed! turn off the event trigger only means, for the next click, nothing would happen as the event is turned off. But for the current round of click event, after this outputResult function is called, it will continue to run the following code in the original event callback function if there's no return!
 //**************************
 
+let gameIsOver = false; // this is to store return value from the click event callback function, remembering if the game has finished or not!
+
+
 $('td').on('click', function(e){
 //----- Step 1, on each click, show input on UI, make sure duplicate clicks handled -----
 
   let xOrY; // store the UI input, X or Y, into a variable for later use!
 
-  if ($(this).html() !== "" ){ // click on any box, if it's not empty - occupied already, return message!
-  $('h2').html('The space is occupied!')
+  if(gameIsOver === true) {
+    $('h2').html('Game is over! Try another round?!')
+    return;
+  }
+  // first to check if the game is over or not,
+  // if yes, it should just return the function, so no need to run the rest code.
+  // if not, can continue the game, and check if a space is occupied or not as below.
 
+  if ($(this).html() !== "" ){
+    // click on any box, if it's not empty - occupied already, return message!
+    $('h2').html('The space is occupied!')
+    return;
+    // if a space is occupied, no need to run the rest code, return callback directly! so can start from the next new click.
+  }
   //-----------------------------------------------------
     // $('.hidden').css('visibility','visible');
-    // instead of using hide/show contents by CSS, can simply add an empty element 'h2' at the beginning, and later add or change .html() contents inside it. Thus, there's no need to change CSS that's separated in another file then.
+    // instead of using hide/show contents by CSS, can simply add an empty element 'h2' at the beginning in html, and later add or change .html() contents inside it. Thus, there's no need to change CSS that's separated in another file then.
   //-----------------------------------------------------
-    return;  // if a space is occupied, no need to run the rest code, return callback directly! so can start from the next new click.
-  }
-  //
-  // if( winningOrNot) {
-  //
-  //   return;
-  // }
+
+  // instead of turning off the event, which as discussed is not ideal for mutiple rounds of game without refreshing the page. Thus, here, for every click, add one more step to check if this game has already finished/ returned or not.
 
 
-  $('h2').html(' '); // in case last click made the hidden part to visible, reset here! if it's still hidden, no harm!
+
+
+  $('h2').html(' ');
+  // when it comes to this step, means, the game hasn't finished.
+  // in case the last click was on an occupied space and added content to h2 element in DOM already, for the current round, need to RESET it to empty, and restart the check if the clicked space this time is occupied or not and move on to this step if not.
   //Q: is this a good approach? kind of repetitive!
-  //A: YES, it's necessary, like reset!!!
+  //A: YES, it's necessary!!!
 
 
   if (validClickNumber % 2 === 0){ // Otherwise, if it's empty, when validClickNumber is even, input 'X', when odd, input 'O'. As validClickNumber starts from 0, so the first input is always 'X'
@@ -95,10 +111,10 @@ $('td').on('click', function(e){
   // console.log(locationX, 'and', locationY); // OK
 
   // use the attribute value X and Y to locate where in the 2D array to store the click content
-  // need to avoid user clicking into the occupied place and changing the value there!!!
-  if (board[locationX][locationY] === '') {
-    board[locationX][locationY] = xOrY;
-  }
+  // since it's already been checked if the clicked space is occupied or not on the above steps, if occupied, it will return the function and won't get to this step.
+  // This, on top of the fact that this click is not on an occupied place, can put the xOrY directly into the board array!
+  board[locationX][locationY] = xOrY;
+
   // console.table(board); //OK
   // so far, corretly store the click content 'X' or 'O' into the right position of the board array!
 
@@ -112,7 +128,7 @@ $('td').on('click', function(e){
 
   // on each direction only need to loop 'board.length'x times, store it in a variable for multiple uses
   const boardLength = board.length;
-  let winningOrNot = false; // this is to store return value from the click event callback function, to pass its final value to UI for further result display.
+
 
   //******* 1, loop through horizontal direction *******
   const arrayForCheck = board[locationX];
@@ -126,8 +142,8 @@ $('td').on('click', function(e){
   }
   // console.log(matchingNumberHorizontal); //OK
   if (matchingNumberHorizontal === boardLength){
-    winningOrNot = true;
-    outputResult(winningOrNot);
+    gameIsOver = true;
+    $('h2').html('Congrats! You won!!!');
     // console.log('winning!'); //OK
     return ;
   }
@@ -144,8 +160,8 @@ $('td').on('click', function(e){
   }
   // console.log(matchingNumberVertical); //OK
   if (matchingNumberVertical === boardLength){
-    winningOrNot = true;
-    outputResult(winningOrNot);
+    gameIsOver = true;
+    $('h2').html('Congrats! You won!!!');
     // console.log('winning!');//OK
     return ;
   }
@@ -166,8 +182,8 @@ $('td').on('click', function(e){
     }
     // console.log(matchingNumberBackwardDiagnal); //OK
     if (matchingNumberBackwardDiagnal === boardLength){
-      winningOrNot = true;
-      outputResult(winningOrNot);
+      gameIsOver = true;
+      $('h2').html('Congrats! You won!!!');
       // console.log('winning!');//OK
       return ;
     }
@@ -179,7 +195,7 @@ $('td').on('click', function(e){
   // then there's no need to check forward diagonal direction!
   // as the only way for this kind of location to win is through horizontal and vertical line,
   // which should have been checked on the above 2x steps!
-  // if not in the center forward diagonal line, after checking the above steps up to here, winningOrNot remains false;
+  // if not in the center forward diagonal line, after checking the above steps up to here, gameIsOver remains false;
   if ((locationX + locationY) === (boardLength - 1)){
     let matchingNumberForwardDiagnal = 0;
 
@@ -190,8 +206,8 @@ $('td').on('click', function(e){
     }
     // console.log(matchingNumberForwardDiagnal); //OK
     if (matchingNumberForwardDiagnal === boardLength){
-      winningOrNot = true;
-      outputResult(winningOrNot);
+      gameIsOver = true;
+      $('h2').html('Congrats! You won!!!');
       // console.log('winning!'); //OK
       return;
     }
@@ -200,28 +216,26 @@ $('td').on('click', function(e){
   // the worst case scenerio, no winner at all, need to return
   // after clicking all the spaces, the total number of spaces in the array should be (array.length ** 2),
   // if there's any winner, it should have returned in previous steps, and the whole click event callback function finishes, also triggers the event to be off.
-  // if there's no winner, it will reach this step, but as it's included inside the event callback for each click, only when the valid click numbers reached (array.length ** 2), meanwhile there's no matching from the above steps, it can determine that there's no winner at the end, Game Over!
+  // if there's no winner, it will reach this step, but as it's included inside the event callback for each click, only when the valid click numbers reached (array.length ** 2), it means all spaces are occupied and there's no matching from the above steps, it's also Game Over!
   if (validClickNumber === boardLength ** 2){
-    outputResult(winningOrNot); // in this step, winningOrNot remain false!
+    gameIsOver = true;
+    $('h2').html('Ooooops! No luck! Try again?!');
+    // in this step, gameIsOver remain false!
     return;
   }
 });
 
 // So far, the above logic should work for multiple line board,
 // we got return value from the click event callback function, which also triggered the output on UI!
-// !!! the winningOrNot variable should sit outside the click event, then can use this returned value to UI intercation.
+// !!! the gameIsOver variable should sit outside the click event, to record if the game has finished or not!
 
 
 
 
 
 
-  // 4>>>> put all check loops into separate functions, make click event callback look neat!
-
-// need to think about output into DOM tomorrow - Done!
-// also separate jQuery with vanilla JS
-// also further refactor
-// finally, decorate UI!
+// 1>>>> put all check loops into separate functions, make click event callback look neat!
+// 2>>>> finally, decorate UI!
 
 
 
