8474749dfb9f69df371901e65836e3929ea122f3
Viola Hu <lovelyviolaat@hotmail.com>
2019-05-22 12:41:20 +1000

    defactor finished so far, will add extra features

--- js/test.js
+++ js/test.js
@@ -1,105 +1,121 @@
 // each item inside this board is board[X][Y], X,Y: 0-2
-const board = [
-  ["","",""],
-  ["","",""],
-  ["","",""]
-]
 
-const boardLength = board.length;
-// calculate valid click number,
-// which can be used to make sure that X and O will take turns to be put into boxes
-let validClickNumber = 0;
+const ticTacToe = {
 
-let xOrY; // store the UI input, X or Y, into a variable for later use!
+  board: [
+    ["","",""],
+    ["","",""],
+    ["","",""]
+  ],
 
-let gameIsOver = false; // this is to store return value from the click event callback function, remembering if the game has finished or not!
+  boardLength: 3,
 
-const putXOrY = function(element){
-  if (validClickNumber % 2 === 0){
-    //  when validClickNumber is even, input 'X', when odd, input 'O'. As validClickNumber starts from 0, so the first input is always 'X'
-    validClickNumber += 1; // update the total number of valid clicks
-    $(element).html('X'); // add 'X' to the clicked td element as content
-    xOrY = 'X'; // update xOrY variable's value, to be used for storing click content into board array later
-  }else {
-    validClickNumber += 1;
-    $(element).html('O');
-    xOrY = 'O';
-  }
-};
+  // calculate valid click number,
+  // which can be used to make sure that X and O will take turns to be put into boxes
+  validClickNumber: 0,
 
+  // store the UI input, X or Y, into a variable for later use!
+  xOrY:"",
 
-const checkHorizontal = function(locationX){
-  let matchingNumberHorizontal = 0;
-  const arrayForCheck = board[locationX];
-
-  for (var i = 0; i < boardLength; i++) {
-    if (arrayForCheck[i] === xOrY){
-      matchingNumberHorizontal += 1;
+  putXOrYIntoDOM: function(){
+    if (this.validClickNumber % 2 === 0){
+      //  when validClickNumber is even, input 'X', when odd, input 'O'. As validClickNumber starts from 0, so the first input is always 'X'
+      this.validClickNumber += 1; // update the total number of valid clicks
+      this.xOrY = 'X';// update xOrY variable's value, to be used for storing click content into board array later
+      return this.xOrY;
+    }else {
+      this.validClickNumber += 1;
+      this.xOrY = 'O';
+      return this.xOrY;
     }
-  }
+  },
 
-  if (matchingNumberHorizontal === boardLength){
-    gameIsOver = true;
-    return gameIsOver;
-  }
-};
+  putXOrYOntoBoard: function(locationX,locationY){
+    this.board[locationX][locationY] = this.xOrY;
+  },
 
+  // this is to store return value from the click event callback function, remembering if the game has finished or not!
+  gameIsOver: false,
 
-const checkVertical = function(locationY){
-  let matchingNumberVertical = 0;
+  checkHorizontal: function(locationX){
+    let matchingNumberHorizontal = 0;
+    const arrayForCheck = this.board[locationX];
 
-  for (var i = 0; i < boardLength; i++) {
-    if(board[i][locationY] === xOrY){
-      matchingNumberVertical += 1;
+    for (var i = 0; i < this.boardLength; i++) {
+      if (arrayForCheck[i] === this.xOrY){
+        matchingNumberHorizontal += 1;
+      }
     }
-  }
 
-  if (matchingNumberVertical === boardLength){
-    gameIsOver = true;
-    return gameIsOver ;
-  }
-};
-
-
-const checkBackwardDiagonal = function (){
-  let matchingNumberBackwardDiagonal = 0;
-
-  for (var i = 0; i < boardLength; i++) {
-    if(board[i][i] === xOrY) {
-      matchingNumberBackwardDiagonal += 1;
+    if (matchingNumberHorizontal === this.boardLength){
+      this.gameIsOver = true;
+      return this.gameIsOver;
     }
-  }
+  },
 
-  if (matchingNumberBackwardDiagonal === boardLength){
-    gameIsOver = true;
-    return gameIsOver;
-  }
-};
+  checkVertical: function(locationY){
+    let matchingNumberVertical = 0;
 
-
-const checkForwardDiagonal = function(){
-  let matchingNumberForwardDiagonal = 0;
-
-  for (var i = 0; i < boardLength; i++) {
-    if(board[i][boardLength - 1 - i] === xOrY) {
-      matchingNumberForwardDiagonal += 1;
+    for (var i = 0; i < this.boardLength; i++) {
+      if(this.board[i][locationY] === this.xOrY){
+        matchingNumberVertical += 1;
+      }
     }
-  }
 
-  if (matchingNumberForwardDiagonal === boardLength){
-    gameIsOver = true;
-    return gameIsOver;
-  }
+    if (matchingNumberVertical === this.boardLength){
+      this.gameIsOver = true;
+      return this.gameIsOver ;
+    }
+  },
+
+  checkBackwardDiagonal: function (){
+    let matchingNumberBackwardDiagonal = 0;
+
+    for (var i = 0; i < this.boardLength; i++) {
+      if(this.board[i][i] === this.xOrY) {
+        matchingNumberBackwardDiagonal += 1;
+      }
+    }
+
+    if (matchingNumberBackwardDiagonal === this.boardLength){
+      this.gameIsOver = true;
+      return this.gameIsOver;
+    }
+  },
+
+  checkForwardDiagonal: function(){
+    let matchingNumberForwardDiagonal = 0;
+
+    for (var i = 0; i < this.boardLength; i++) {
+      if(this.board[i][this.boardLength - 1 - i] === this.xOrY) {
+        matchingNumberForwardDiagonal += 1;
+      }
+    }
+
+    if (matchingNumberForwardDiagonal === this.boardLength){
+      this.gameIsOver = true;
+      return this.gameIsOver;
+    }
+  },
+
+  checkIfNoWinning: function(){
+    if (this.validClickNumber === this.boardLength ** 2){
+      this.gameIsOver = true;
+      return this.gameIsOver;
+    }
+  },
+
 };
 
 
+// click event, for each click, what to do!
 $('td').on('click', function(e){
   //****** Step 1, on each click, show input on UI, handle game over situation and clicks on occupied spaces ******
   // first to check if the game is over or not,
   // if yes, it should just return the function, so no need to run the rest code.
   // if not, can continue the game, and check if a space is occupied or not as below.
-  if(gameIsOver === true) {
-    $('h2').html('Game is over! Try another round?!')
+  if(ticTacToe.gameIsOver === true) {
+    $('h2').html('Game is over! Another round?!')
     return;
   }
 
@@ -113,12 +129,10 @@ $('td').on('click', function(e){
   // in case the last click was on an occupied space and added content to h2 element in DOM already, for the current round, need to RESET it to empty, and restart the check if the clicked space this time is occupied or not and move on to this step if not.
   $('h2').html(' ');
 
-
   // call pre-defined function to put 'X' or 'Y' into UI/DOM
-  putXOrY(this);
+  $(this).html(ticTacToe.putXOrYIntoDOM());
 
-
-  //****** Step 2, store the UI input variable into board array ******
+  //****** Step 2, store the UI input into board array ******
   // get the click event object data - target property, it prints out the pure DOM element that user just clicked, including all its ATTRIBUTEs and contents!
   // store this target DOM element into a variable to utilise its id attribute!!!
   const clickedTarget = e.target;
@@ -131,8 +145,9 @@ $('td').on('click', function(e){
 
   // use the attribute value X and Y to locate where in the 2D array to store the click content
   // since it's already been checked if the clicked space is occupied or not on the above steps, if occupied, it will return the function and won't get to this step.
-  // This, on top of the fact that this click is not on an occupied place, can put the xOrY directly into the board array!
-  board[locationX][locationY] = xOrY;
+  // This, on top of the fact that this click is not on an occupied place, can put the this.xOrY directly into the board array!
+  // use pre-determined game object function to store the input into board array of the game object.
+  ticTacToe.putXOrYOntoBoard(locationX, locationY);
 
   //****** Step 3, check matching spaces and decide if any winning ******
   //**************** Core Logic *******************
@@ -142,13 +157,13 @@ $('td').on('click', function(e){
   // on each direction only need to loop 'board.length'x times, store it in a variable for multiple uses
 
   //******* 1) loop through horizontal direction *******
-  if (checkHorizontal(locationX)){
+  if (ticTacToe.checkHorizontal(locationX)){
     $('h2').html('Congrats! You won!!!');
     return;
   }
 
   //******* 2) loop through vertical direction *******
-  if (checkVertical(locationY)){
+  if (ticTacToe.checkVertical(locationY)){
     $('h2').html('Congrats! You won!!!');
     return;
   }
@@ -159,47 +174,59 @@ $('td').on('click', function(e){
   // as the only way for this kind of location to win is through horizontal and vertical line,
   // which should have been checked on the above 2x steps!
   if (locationX === locationY) {
-    if(checkBackwardDiagonal()) {
+    if(ticTacToe.checkBackwardDiagonal()) {
       $('h2').html('Congrats! You won!!!');
       return;
     }
   }
 
   //******* 4) loop through forward diagonal direction '/' *******
-  if ((locationX + locationY) === (boardLength - 1)){
-
-    if(checkForwardDiagonal()){
+  if ((locationX + locationY) === (ticTacToe.boardLength - 1)){
+    if(ticTacToe.checkForwardDiagonal()){
       $('h2').html('Congrats! You won!!!');
       return;
     }
-
   }
 
   // ****** 5) no winning after all *******
-  if (validClickNumber === boardLength ** 2){
-    gameIsOver = true;
-    $('h2').html('Ooooops! No luck! Try again?!');
+  if(ticTacToe.checkIfNoWinning()){
+    $('h2').html('Drew!');
     return;
   }
-
 });
-
-
-
 // So far, the above logic should work for multiple line board,
-// we got return value from the click event callback function, which also triggered the output on UI!
-// !!! the gameIsOver variable should sit outside the click event, to record if the game has finished or not!
+
+
+
+
+
+// event callback, on each event occasion
+// - retrieves DOM data
+// - pass the retrieved DOM data to core logic functions (stored in game object) which determine whether to change something and return value accordingly
+// - based on the return value from the core logic functions, event callback communicates with DOM to make the actual change happen in DOM/UI
 
 
 
 
 
 
-// 1>>>> put all check loops into separate functions inside an object as methods, make click event callback look neat!
+
+// 1>>>> put all check loops into separate functions inside an object as methods, make click event callback look neat! -- done!
 
 // 2>>>> think about next like another round!
 
 // 3>>>> finally, decorate UI!
+
+
+
+
+
+
+
+
+
+
+
 
 
 
